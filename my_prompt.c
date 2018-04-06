1c003aa66cb86b020f7e607b1289fa5694115f03
Cristiana Morais <up201505454@fc.up.pt>
2018-04-06 15:42:58 +0100

    Update my_prompt.c

--- my_prompt.c
+++ my_prompt.c
@@ -24,10 +24,10 @@
 #define PIPE_WRITE 1
 
 typedef struct command {
-    char *cmd;              // string apenas com o comando
-    int argc;               // número de argumentos
-    char *argv[MAXARGS+1];  // vector de argumentos do comando
-    struct command *next;   // apontador para o próximo comando
+  char *cmd;              // string apenas com o comando
+  int argc;               // número de argumentos
+  char *argv[MAXARGS+1];  // vector de argumentos do comando
+  struct command *next;   // apontador para o próximo comando
 } COMMAND;
 
 // variáveis globais
@@ -36,10 +36,11 @@ char* outputfile = NULL;    // nome de ficheiro (em caso de redireccionamento da
 int background_exec = 0;    // indicação de execução concorrente com a mini-shell (0/1)
 
 // declaração de funções
-COMMAND* parse(char *);
-void print_parse(COMMAND *);
-void execute_commands(COMMAND *);
-void free_commlist(COMMAND *);
+COMMAND* parse(char* linha);
+void print_parse(COMMAND* commlist);
+void execute_commands(COMMAND* commlist);
+void free_commlist(COMMAND* commlist);
+void child_handler(int sig);
 
 // include do código do parser da linha de comandos
 #include "parser.c"
@@ -85,18 +86,100 @@ void print_parse(COMMAND* commlist) {
   printf("---------------------------------------------------------\n");
 }
 
-
-void free_commlist(COMMAND *commlist){
-    // ...
-    // Esta função deverá libertar a memória alocada para a lista ligada.
-    // ...
+void child_handler(int sig) {
+  if(sig==SIGCHLD)
+    waitpid(-1,NULL,WNOHANG);
 }
 
+void free_commlist(COMMAND *commlist){
+  // ...
+  // Esta função deverá libertar a memória alocada para a lista ligada.
+  // ...
+
+  COMMAND* tmp;
+  while(commlist!=NULL) {
+    tmp= commlist->next;
+    free(commlist);
+    commlist=tmp;
+  }
+}
 
 void execute_commands(COMMAND *commlist) {
-    // ...
-    // Esta função deverá "executar" a "pipeline" de comandos da lista commlist.
-    // ...
-} 
+  // ...
+  // Esta função deverá "executar" a "pipeline" de comandos da lista commlist.
+  // ...
+  
+  int pid, fd[2], fdold[2], filepointer, status;
+  COMMAND* liststart = commlist;
 
+  if(strcmp(commlist->argv[0],"exit")==0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  if(background_exec)
+    signal(SIGCHLD,child_handler);
+  else
+    signal(SIGCHLD,SIG_IGN);
+
+  while(commlist!=NULL) {
+    fdold[0]=fd[0];
+    fdold[1]=fd[1];
+
+    if(commlist->next!=NULL) {
+      if(pipe(fd)==-1) {
+        printf("Pipe Error\n");
+      }
+    }
+
+    if((pid=fork())==-1) {
+      printf("Fork Error\n");
+    }
+
+    if(pid) {
+      if(liststart!=commlist) {
+	close(fdold[0]);
+	close(fdold[1]);
+      }
+      commlist=commlist->next;
+    }
+
+    else {
+      if(liststart!=commlist) {
+	dup2(fdold[0],0);
+	close(fdold[0]);
+	close(fdold[1]);
+      }
+
+      if(commlist->next!=NULL) {
+	dup2(fd[1],1);
+	close(fd[0]);
+	close(fd[1]);
+      }
+
+      if(commlist==liststart && inputfile != NULL) {
+	filepointer = open(inputfile,O_RDONLY);
+	if(filepointer==-1) {
+	  printf("Filepointer Error\n");
+	}
+	dup2(filepointer,0);
+	close(filepointer);
+      }
+
+      if(commlist->next==NULL && outputfile!=NULL) {
+	filepointer = open(outputfile,O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if(filepointer==-1) {
+	  printf("Filepointer Error\n");
+	}
+	dup2(filepointer,1);
+	close(filepointer);
+      }
+
+      execvp(commlist->cmd,commlist->argv);
+    }
+  }
+
+  if(background_exec==0) {
+    waitpid(pid,&status, WUNTRACED| WCONTINUED);
+  }
+}
 
